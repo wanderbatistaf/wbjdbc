196853320b4a6651c3f82a120d61d7a02657d093
wanderbatistaf <98125036+wanderbatistaf@users.noreply.github.com>
2025-01-06 16:45:31 +0000

    Adicionado suporte para múltiplos sistemas operacionais e novas dependências.

--- setup.py
+++ setup.py
@@ -7,14 +7,36 @@ import pathlib
 current_dir = pathlib.Path(__file__).parent.resolve()
 long_description = (current_dir / "README.md").read_text(encoding="utf-8")
 
+import platform
+import sys
+
+
 def install_dependencies():
     try:
         import jpype
         print("JPype1 já está instalado.")
     except ImportError:
-        wheel_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'wbjdbc', 'resources', 'dependencies', 'jpype1-1.5.1-cp312-cp312-win_amd64.whl'))
-        print(f"Instalando JPype1 de {wheel_path}")
-        subprocess.check_call(['pip', 'install', wheel_path])
+        system = platform.system().lower()
+        python_version = f"cp{sys.version_info.major}{sys.version_info.minor}"
+        dependencies_dir = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "wbjdbc", "resources", "dependencies"))
+
+        if system == "windows":
+            wheel_file = f"jpype1-1.5.1-{python_version}-{python_version}-win_amd64.whl"
+        elif system == "linux":
+            wheel_file = f"jpype1-1.5.1-{python_version}-{python_version}-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
+        elif system == "darwin":
+            wheel_file = f"jpype1-1.5.1-{python_version}-{python_version}-macosx_10_9_universal2.whl"
+        else:
+            raise RuntimeError(f"Sistema operacional não suportado: {system}")
+
+        wheel_path = os.path.join(dependencies_dir, wheel_file)
+
+        if not os.path.exists(wheel_path):
+            raise FileNotFoundError(f"O arquivo {wheel_file} não foi encontrado no diretório {dependencies_dir}.")
+
+        print(f"Instalando JPype1 de {wheel_path} para {system}.")
+        subprocess.check_call(["pip", "install", wheel_path])
 
 
 # Chama a função para instalar JPype1
